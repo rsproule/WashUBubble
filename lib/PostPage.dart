522f5682c6a1c15440f4d28d0623f9e1dbafcb5b
Ryan Sproule <rsproule23@gmail.com>
2017-06-06 22:08:28 -0400

    stuff works, but very glitchy

--- lib/PostPage.dart
+++ lib/PostPage.dart
@@ -25,32 +25,40 @@ class PostPageState extends State<PostPage> {
       .child("threadNodes");
   List<DataSnapshot> threads;
   PageController pageController = new PageController();
-  List<replyTile.ReplyTile> threadTiles = [];
+  List<Widget> threadTiles = [];
   bool hasChildren;
 
 
   PostPageState(DataSnapshot s) {
     this.postSnapshot = s;
 
-    _getThreads(replyReference.child(this.postSnapshot.key)); //all the replies in this post
+    _getThreads(replyReference.child(
+        this.postSnapshot.key)); //all the replies in this post
   }
 
   _getThreads(DatabaseReference ref) async {
     DataSnapshot s = await ref.once();
-    Map m = s.value;   //all the replies in this post in a map
-    m.forEach((k, v) async {
-      DataSnapshot snap = await ref.child(k).once();
-      Animation<double> animation1;
-      replyTile.ReplyTile childTile;
-      if(v['parent'] == this.postSnapshot.key) {
-         childTile = new replyTile.ReplyTile(
-            snap, animation1, this.postSnapshot.key);
-         setState(() {
-           this.threadTiles.add(childTile);
-         });
-      }
-    });
+    Map m = s.value; //all the replies in this post in a map
+    List<Widget> newTiles = [];
+    if (m != null) {
+      m.forEach((k, v) async {
+        DataSnapshot snap = await ref.child(k).once();
+        Animation<double> animation1;
+        replyTile.ReplyTile childTile;
+        if (v['parent'] == this.postSnapshot.key) {
+          childTile = new replyTile.ReplyTile(
+              snap, animation1, this.postSnapshot.key);
 
+          newTiles.add(childTile);
+          newTiles.add(new Divider());
+        }
+
+//      newTiles.sort((a, b) => (a.snapshot.key.compareTo(b.snapshot.key)));
+        setState(() {
+          this.threadTiles = newTiles;
+        });
+      });
+    }
   }
 
   @override
@@ -159,57 +167,113 @@ class PostPageState extends State<PostPage> {
 
 
                     //Main response field:
-                    new Row(
-                        children: <Widget>[
-                          new Flexible(
-                              child: new TextField(
-                                  onChanged: (String val) {
-                                    setState(() {
-                                      this.replyIsFilled = val != "";
-                                    });
-                                  },
-                                  controller: replyController,
-                                  maxLines: 3,
-                                  decoration: new InputDecoration(
-                                      hideDivider: true,
-                                      hintText: "Add a Response",
-                                      icon: new Icon(Icons.reply)
-                                  )
-                              )
-                          ),
 
-                          new MaterialButton(
-                              child: new Text("Reply"),
-                              splashColor: Colors.blue,
-                              textColor: this.replyIsFilled
-                                  ? Colors.blue
-                                  : Colors
-                                  .grey,
-                              onPressed: this.replyIsFilled
-                                  ? _sendPostResponse
-                                  : null
-                          )
-                        ]
+                    new Center(
+                        child: new MaterialButton(
+                            child: new Text("Add a response"),
+                            color: Colors.blue,
+                            textColor: Colors.white,
+                            onPressed: () {
+                              showDialog(
+                                  context: context,
+                                  child: new Dialog(
+                                    child: new Row(
+                                        children: <Widget>[
+                                          new Flexible(
+                                              child: new TextField(
+                                                  onChanged: (String val) {
+                                                    setState(() {
+                                                      this.replyIsFilled =
+                                                          val != "";
+                                                    });
+                                                  },
+                                                  controller: replyController,
+                                                  maxLines: 10,
+                                                  decoration: new InputDecoration(
+                                                      hideDivider: true,
+                                                      hintText: "Add a Response",
+                                                      icon: new Icon(
+                                                          Icons.reply)
+                                                  )
+                                              )
+                                          ),
+
+                                          new MaterialButton(
+                                              child: new Text("Reply"),
+                                              splashColor: Colors.blue,
+                                              textColor: Colors.blue,
+                                              onPressed: () {
+                                                if (tryPost()) {
+                                                  Navigator.of(context).pop();
+                                                } else {
+                                                  print("failed to post");
+                                                }
+                                              }
+
+                                          )
+                                        ]
+                                    ),
+                                  )
+                              );
+                            }
+                        )
+
                     ),
 
 
-                    new Divider(),
+                    new Divider(height: 30.0),
+
+                    new Divider(height: 140.0, color: Colors.transparent),
+
+                    new Column(
+
+                        children: <Widget>[
+
+                          new Center(
+                              child: new Text("Swipe to view responses.")
+                          ),
+                          new Divider(color: Colors.transparent),
+                          new Center(
+                              child: new Icon(Icons.arrow_downward)
+                          ),
+
+                        ]
+                    )
 
                   ]),
 
 
               //All the root thread nodes for this post:
+              new Container(
+                  padding: const EdgeInsets.only(top: 50.0),
+                  child: new ListView(
 
-
-              new Column(
-                  children: this.threadTiles != null
-                      ? this.threadTiles
-                      : new Text("Be the first to Respond")
+                      children: this.threadTiles.length != 0
+                          ? this.threadTiles
+                          : <Widget>[
+                        new Center(
+                            child: new Padding(
+                                padding: const EdgeInsets.all(40.0),
+                                child: new Text("Be the first to Respond")
+                            )
+                        )
+                      ]
+                  )
               )
-
             ]
         )
     );
+  }
+
+  bool tryPost() {
+    String reply = replyController.text;
+
+    if (reply != "") {
+      _sendPostResponse();
+      return true;
+    } else {
+      return false;
+    }
   }
 
   _sendPostResponse() async {
@@ -218,18 +282,24 @@ class PostPageState extends State<PostPage> {
         .getUser()
         .currentUser;
 
-    await replyReference.child(this.postSnapshot.key).push().set({
-      'parent': this.postSnapshot.key,
-      'content': reply,
-      'children': {
-        'hasChildren': false
-      },
-      'username': user.displayName,
-      'photo_url': user.photoUrl,
-      'user_id': user.id,
-    });
+    if (reply != "") {
+      await replyReference.child(this.postSnapshot.key).push().set({
+        'parent': this.postSnapshot.key,
+        'content': reply,
+        'children': {
+          'hasChildren': false
+        },
+        'username': user.displayName,
+        'photo_url': user.photoUrl,
+        'user_id': user.id,
+      });
 
-    replyController.clear();
+      replyController.clear();
+
+      //update state:
+      _getThreads(replyReference.child(
+          this.postSnapshot.key));
+    }
   }
 
 
--- lib/ReplyTile.dart
+++ lib/ReplyTile.dart
@@ -5,18 +5,18 @@ import 'package:google_sign_in/google_sign_in.dart';
 import './loginStuff.dart' as login;
 
 class ReplyTile extends StatefulWidget {
-  DataSnapshot s;
+  DataSnapshot snapshot;
   Animation a;
   String postKey;
 
   ReplyTile(DataSnapshot s, Animation a, String parent) {
-    this.s = s;
+    this.snapshot = s;
     this.a = a;
     this.postKey = parent;
   }
 
   @override
-  ReplyTileState createState() => new ReplyTileState(s, a, postKey);
+  ReplyTileState createState() => new ReplyTileState(snapshot, a, postKey);
 }
 
 class ReplyTileState extends State<ReplyTile> {
@@ -52,8 +52,9 @@ class ReplyTileState extends State<ReplyTile> {
     DataSnapshot s = await ref.once();
     Animation<double> animation1;
     ReplyTile childTile = new ReplyTile(s, animation1, this.postKey);
+    this.childrenTiles.add(childTile);
     setState(() {
-      this.childrenTiles.add(childTile);
+      this.childrenTiles = childrenTiles;
     });
 //    print(s.value['content']);
   }
@@ -70,36 +71,43 @@ class ReplyTileState extends State<ReplyTile> {
                         onPressed: () {
                           showDialog(
                               context: context,
-                              child:
-                              new Dialog(child:
-                              new Container(
-                                  height: 300.0,
-                                  child: new Column(
-                                      children: <Widget>[
-                                        new TextField(
-                                            controller: replyController,
-                                            decoration: new InputDecoration(
-                                              hintText: "Response Message",
-                                              icon: new Icon(Icons.reply),
-                                              hideDivider: true,
+                              child: new Dialog(
+                                child: new Row(
+                                    children: <Widget>[
+                                      new Flexible(
+                                          child: new TextField(
+                                              onChanged: (String val) {
+//                                                setState(() {
+//                                                  replyIsFilled =
+//                                                      val != "";
+//                                                });
+                                              },
+                                              controller: replyController,
+                                              maxLines: 10,
+                                              decoration: new InputDecoration(
+                                                  hideDivider: true,
+                                                  hintText: "Add a Response",
+                                                  icon: new Icon(
+                                                      Icons.reply)
+                                              )
+                                          )
+                                      ),
 
-                                            )
-                                        ),
-                                        new Divider(),
+                                      new MaterialButton(
+                                          child: new Text("Reply"),
+                                          splashColor: Colors.blue,
+                                          textColor: Colors.blue,
+                                          onPressed: () {
+                                            if (tryPost()) {
+                                              Navigator.of(context).pop();
+                                            } else {
+                                              print("failed to post");
+                                            }
+                                          }
 
-                                        new MaterialButton(
-                                            color: Colors.blue,
-                                            textColor: Colors.white,
-                                            child: new Text("Send"),
-                                            onPressed:
-                                            _sendReply
-
-                                        )
-                                      ]
-                                  )
-                              )
-
-
+                                      )
+                                    ]
+                                ),
                               )
                           );
                         }
@@ -161,11 +169,25 @@ class ReplyTileState extends State<ReplyTile> {
     );
   }
 
+  tryPost(){
+    String content = replyController.text;
+
+    if(content == ""){
+      return false;
+    }else{
+      _sendReply();
+      return true;
+    }
+  }
+
   _sendReply() async {
 
     threadInPostReference = FirebaseDatabase.instance.reference().child("threadNodes").child(
-            snapshot.value['parent']); // the post container
+            this.postKey); // the post container
     String content = replyController.text;
+
+
+
     GoogleSignInAccount user = login
         .getUser()
         .currentUser;
@@ -185,21 +207,39 @@ class ReplyTileState extends State<ReplyTile> {
     });
 
 
+
+    Map newChildren = snapshot.value['children'];
+    newChildren.putIfAbsent(newKey, ()=>true);
+    newChildren.remove("hasChildren");
+    newChildren.putIfAbsent("hasChildren", ()=>true);
+
+
     //update the parents children
     await threadInPostReference.child(this.snapshot.key).set({
       //this info is for the new response, look up how to do update!!! --> cant find anything :(
       //this deletes everything that is there... i think we can replace with all snapshot data np
       "parent": snapshot.value['parent'],
       "content": snapshot.value['content'],
-      'children': { //new
-        'hasChildren': true,
-        newKey: true
-      },
+      'children': newChildren,
       'username': snapshot.value['username'],
       'photo_url': snapshot.value['photo_url'],
       'user_id': snapshot.value['user_id']
     });
 
     replyController.clear();
+
+
+    Map m = snapshot.value['children'];
+    m.forEach((k, v) {
+      if (k == 'hasChildren') {
+        hasChildren = v;
+      } else {
+        threadChildren.add(k);
+        DatabaseReference ref = FirebaseDatabase.instance.reference().child(
+            "threadNodes")
+            .child(postKey).child(k);
+        getChildren(ref);
+      }
+    });
   }
 }
